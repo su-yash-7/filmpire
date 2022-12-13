b33dcd2fddda0841f98f4a9dc7f3ebf5505e097a
su-yash-7 <suyashpradhan7@gmail.com>
2022-12-13 15:21:08 +0530

    Created Basic Navigation Bar Layout

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,10 +1,73 @@
 import React from 'react';
-
+import {
+  AppBar,
+  IconButton,
+  Toolbar,
+  Drawer,
+  Button,
+  Avatar,
+  useMediaQuery,
+} from '@mui/material';
+import {
+  Menu,
+  AccountCircle,
+  Brightness4,
+  Brightness7,
+} from '@mui/icons-material';
+import { Link } from 'react-router-dom';
+import { ClassNames } from '@emotion/react';
+import useStyles from './styles';
+import { useTheme } from '@mui/material/styles';
 const NavBar = () => {
+  const classes = useStyles();
+  const isMobile = useMediaQuery('(max-width: 600px)');
+  const theme = useTheme();
+  const isAuthenticated = true;
   return (
-    <div>
-      <h1>NavBar</h1>
-    </div>
+    <>
+      <AppBar>
+        <Toolbar className={classes.toolbar}>
+          {isMobile && (
+            <IconButton
+              color="inherit"
+              edge="start"
+              style={{ outline: 'none' }}
+              onClick={() => {}}
+              className={classes.menuButton}
+            >
+              <Menu />
+            </IconButton>
+          )}
+          <IconButton color="inherit" sx={{ ml: 1 }} onClick={() => {}}>
+            {theme.palette.mode === 'dark' ? <Brightness4 /> : <Brightness7 />}
+          </IconButton>
+          {!isMobile && 'search...'}
+          <div>
+            {!isAuthenticated ? (
+              <Button color="inherit" onClick={() => {}}>
+                Login &nbsp; <AccountCircle />
+              </Button>
+            ) : (
+              <Button
+                color="inherit"
+                onClick={() => {}}
+                component={Link}
+                to={`/profile/:id`}
+                className={classes.linkButton}
+              >
+                {!isMobile && <>My Movies &nbsp;</>}
+                <Avatar
+                  style={{ width: 30, height: 30 }}
+                  alt="profile"
+                  src="https://www.w3schools.com/howto/img_avatar.png"
+                />
+              </Button>
+            )}
+          </div>
+          {isMobile && 'search...'}
+        </Toolbar>
+      </AppBar>
+    </>
   );
 };
 
